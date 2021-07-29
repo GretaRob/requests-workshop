e0e7e4fb608c607d2178d97fd5fdd28e1737e61e
GretaRob <greta.robichaud@gmail.com>
2021-07-29 17:06:30 +0700

    additional apie tests

--- exercises/exercises_01.py
+++ exercises/exercises_01.py
@@ -13,7 +13,7 @@ def test_check_status_code_equals_200():
 # Check that the value of the response header 'Content-Type' equals 'application/json'
 
 
-def test_check_header_equals_json():
+def test_check_content_type_equals_json():
     response = requests.get("http://api.zippopotam.us/us/90210")
     assert response.headers['Content-Type'] == 'application/json'
 
@@ -21,20 +21,33 @@ def test_check_header_equals_json():
 # Perform a GET request to http://api.zippopotam.us/us/90210
 # Check that the response body encoding is not set (equal to None)
 
+def test_check_body_encoding_is_not_set():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    assert response.encoding == None
 
 # Exercise 1.4
 # Perform a GET request to http://api.zippopotam.us/us/90210
 # Check that the response body element 'country' has a value equal to 'United States'
-
+def test_check_country_equals_united_states():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    response_body = response.json()
+    assert response_body["country"] == "United States"
 
 # Exercise 1.5
 # Perform a GET request to http://api.zippopotam.us/us/90210
 # Check that the first 'place name' element in the list of places
 # has a value equal to 'Beverly Hills'
-
+def test_check_place_name_equals_beverly_hills():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    response_body = response.json()
+    assert response_body["places"][0]["place name"] == "Beverly Hills"
 
 # Exercise 1.6
 # Perform a GET request to http://api.zippopotam.us/us/90210
 # Check that the response body element 'places' has an array
 # value with a length of 1 (i.e., there's one place that corresponds
 # to the US zip code 90210)
+def test_check_places_number_equals_1():
+    response = requests.get("http://api.zippopotam.us/us/90210")
+    response_body = response.json()
+    assert len(response_body["places"]) == 1

2f5a9ea14563626ce942995c50d4624468deb4fb
GretaRob <greta.robichaud@gmail.com>
2021-08-11 19:01:57 +0700

    testing with external data sources

--- exercises/exercises_02.py
+++ exercises/exercises_02.py
@@ -1,4 +1,6 @@
-import pytest, requests, csv
+import pytest
+import requests
+import csv
 
 # Exercise 2.1
 # Create a test data object test_data_zip
@@ -7,6 +9,8 @@ import pytest, requests, csv
 #           us -    90210 - Beverly Hills
 #           it -    50123 - Firenze
 #           ca -      Y1A - Whitehorse
+test_data_zip = [('us', '90210', 'Beverly Hills'),
+                 ('it', '50123', 'Firenze'), ('ca', 'Y1A', 'Whitehorse')]
 
 
 # Exercise 2.2
@@ -14,7 +18,12 @@ import pytest, requests, csv
 # a GET call to http://api.zippopotam.us/<country_code>/<zip_code>
 # and checks that the values for the 'place name' elements correspond
 # to those that are specified in the test data object
-
+@pytest.mark.parametrize("country_code, zip_code, place_name", test_data_zip)
+def test_get_data_for_zip_check_name(country_code, zip_code, place_name):
+    response = requests.get(
+        f"http://api.zippopotam.us/{country_code}/{zip_code}")
+    response_body = response.json()
+    assert response_body['places'][0]["place name"] == place_name
 
 # Exercise 2.3
 # Create the same test data as above, but now in a .csv file, for example:
@@ -23,12 +32,27 @@ import pytest, requests, csv
 # ca,Y1A,Whitehorse
 # Place this .csv file in the answers folder of the project
 
-
 # Exercise 2.4
 # Create a method read_data_from_csv() that reads the file from 2.3 line by line
 # and creates and returns a test data object from the data in the .csv file
 
 
+def read_data_from_csv():
+    test_data_places_from_csv = []
+    with open("exercises/test_data_places.csv", newline="") as csvfile:
+        data = csv.reader(csvfile, delimiter=",")
+        for row in data:
+            test_data_places_from_csv.append(row)
+    return test_data_places_from_csv
+
 # Exercise 2.5
 # Change the data driven test from Exercise 2.2 so that it uses the test data
 # from the .csv file instead of the test data that was hard coded in this file
+
+
+@pytest.mark.parametrize("country_code, zip_code, place_name", read_data_from_csv())
+def test_get_data_for_zip_check_name(country_code, zip_code, place_name):
+    response = requests.get(
+        f"http://api.zippopotam.us/{country_code}/{zip_code}")
+    response_body = response.json()
+    assert response_body['places'][0]["place name"] == place_name
